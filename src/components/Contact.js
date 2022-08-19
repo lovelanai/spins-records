c3859823e324c5b8c6eb270ddc30f0d493141257
lovelanai <94110810+lovelanai@users.noreply.github.com>
2022-08-19 16:15:16 +0200

    webpage is now full responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import "./Contact.css";
 import "./Mobile.css";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import { Element } from "react-scroll";
 export default function Store() {
   return (
@@ -11,13 +12,19 @@ export default function Store() {
             <p>Email address</p>
             <Form.Control placeholder="name@example.com" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Group
+            className="contact-input"
+            controlId="exampleForm.ControlTextarea1"
+          >
             <p>Ask us a question</p>
             <Form.Control
               placeholder="Ask a question here"
               as="textarea"
               rows={10}
             />
+            <button className="button" variant="primary">
+              Submit
+            </button>
           </Form.Group>
           <h3>We normally awnser within 2 business days</h3>
         </Form>
